366c76a9913feb672e0d3e0b6fe06f48e37a6f50
Zihan Jin <admin@zihanjin.com>
2026-05-17 01:12:52 +1000

    move out of lib

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,2 @@
-#[derive(pest_derive::Parser)]
-#[grammar = "ff.pest"]
-pub struct FFParser;
+mod ast;
+mod parser;
---
+++ src/parser.rs
@@ -0,0 +1,3 @@
+#[derive(pest_derive::Parser)]
+#[grammar = "ff.pest"]
+pub struct FFParser;
